081b322f621c085b2a9e6854723c1795194669a8
Lukas Willems <lukas.willems@fom-net.de>
2023-11-27 16:18:30 +0100

    add: geheimzahl component

---
+++ willems_mc.py
@@ -0,0 +1,14 @@
+geheimzahl = 15
+eingabe = 0
+zaehler = 0
+
+while eingabe != geheimzahl:
+	eingabe = int(input("Bitte eine Ganzzahl eingeben: "))
+
+	if eingabe < geheimzahl:
+		print("Ihre Eingabe war zu klein")
+		zaehler = zaehler +1
+	if eingabe > geheimzahl:
+		print("Ihre Eingabe war zu groß")
+
+print("Sie haben die Zahl erraten")
